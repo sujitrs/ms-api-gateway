3edf247810543c8d69d1e87013c24cc063189364
sujitrs <suryawanshisujeet@gmail.com>
2019-02-08 17:40:35 +0530

    working

--- src/main/java/org/sj/msapigateway/MsApiGatewayApplication.java
+++ src/main/java/org/sj/msapigateway/MsApiGatewayApplication.java
@@ -2,8 +2,12 @@ package org.sj.msapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
